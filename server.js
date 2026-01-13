118d1ca7afb88f784f688d3117bf66ba519809fa
Jayesh0709 <gehlotjayesh65@gmail.com>
2026-01-13 22:05:44 +0530

    routing

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static("public"))
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("index")
+})
+
+app.post("/submit", (req, res) => {
+    // const { name, email, number, msg } = req.body;
+    // console.log(name, email, number, msg);
+    console.log(req.body);
+    res.send(`
+        <script>
+        alert("thanks for submission"); 
+        window.location = "/";
+        </script>
+        `);
 })
 
 app.listen(port, () => {
